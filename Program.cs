6fb0129d85970860f7a337a65db3204d7a276365
Menshikov Vlad <v.menshikov@solutionfactory.ru>
2021-11-19 19:32:06 +0700

    Init

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TestGeekBrains
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
